fc4084fdbd2fe41032522f8f2345173f6737acf4
Bill G <gbilly3@gmail.com>
2019-10-08 20:19:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // // Code your solutions in this file
-// 
+//
 // function writeCards(array, event){
 //   ray = []
 // for (let i = 0; i < array.length; i++){
-// 
+//
 //   ray.push('Thank you, ${array[i]}, for the wonderful ${event} gift!')
 // }
 // return ray
