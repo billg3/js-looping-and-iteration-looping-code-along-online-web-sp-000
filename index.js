33cb3746fbf7403caab547b1d80dda0d130922e4
Bill G <gbilly3@gmail.com>
2019-10-08 20:13:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+  ray = []
+for (let i = 0; i < array.length; i++){
+  console.log("Thank you, ${person}, for the wonderful birthday gift!",)
+}
+  
+}
