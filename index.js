0c8d81064ed68f7af497a2ca629bd9fdee8107ca
Bill G <gbilly3@gmail.com>
2019-10-08 20:18:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+// 
+// function writeCards(array, event){
+//   ray = []
+// for (let i = 0; i < array.length; i++){
+// 
+//   ray.push('Thank you, ${array[i]}, for the wonderful ${event} gift!')
+// }
+// return ray
+// }
 
-function writeCards(array, event){
-  ray = []
-for (let i = 0; i < array.length; i++){
-
-  ray.push('Thank you, ${array[i]}, for the wonderful ${event} gift!')
-}
-return ray
-}
-
-function writeCards( namesArray, event ) {
-  let thankYouCards = []
-  for ( let i = 0; i < namesArray.length; i++ ) {
-    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+function writeCards( array, event ) {
+  let Cards = []
+  for ( let i = 0; i < array.length; i++ ) {
+    Cards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
   return thankYouCards
 }
