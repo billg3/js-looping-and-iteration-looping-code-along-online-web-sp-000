72c7ff734a67085f7213ffc94032bb369a88aae7
Bill G <gbilly3@gmail.com>
2019-10-08 20:17:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ for (let i = 0; i < array.length; i++){
 }
 return ray 
 }
+
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
